942c98829f2e806df3508d47fbe06ad2b2fe45f8
Jacob Dent <murkymicrobe88@gmail.com>
2025-02-28 17:47:44 -0500

    Should take the label.csv and put it into the annotations that the YOLO model will need

---
+++ src/Model_Training/datasets/yolo_annotation_builder.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import os
+import csv
+
+
+def main():
+    
+    # Load label.csv as dataframe
+
+    # Loop over each item
+
+        # grab the current line of the dataframe
+
+        # build name of .txt file
+
+        # Find x center and y center
+
+        # normalize all the data
+
+        # save as .txt file
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+
+
+
+
+
+    main()
